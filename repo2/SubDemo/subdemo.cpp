0085ea10ecad57e8eae38211b623258c9f23551a
Jason Rich Darmawan <63768126+jasonrichdarmawan@users.noreply.github.com>
2023-04-03 09:00:12 +0700

    add example of sub folder

--- repo2/SubDemo/subdemo.cpp
+++ repo2/SubDemo/subdemo.cpp
@@ -1,9 +1,11 @@
 #include "subdemo.hpp"
-#include <opencv2/core.hpp>
+// target_link_libraries(Hello ${OpenCV_LIBS}) on repo1/Hello/CMakeLists.txt 
+// do not make you able to use OpenCV library right away on repo2.
+// #include <opencv2/core.hpp>
 #include <iostream>
 
 void SubDemo::PrintSubDemo()
 {
     std::cout << "SubDemo" << std::endl;
-    cv::Rect rect{0, 0, 0, 0};
+    // cv::Rect rect{0, 0, 0, 0};
 }
